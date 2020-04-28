2ea67c6d3ed7132aa12a436e039b2a7d36c6ead7
Camden Wagner <camden.i.wagner@ivarcode.net>
2020-04-27 23:50:03 -0400

    piece layout

--- src/app/lib/NAW.library.ts
+++ src/app/lib/NAW.library.ts
@@ -5,6 +5,16 @@ export const enum Terrain {
     Water
 }
 
+export const enum Units {
+    None,
+    Infantry1,
+    Anti_Tank1,
+    Tank1,
+    Infantry2,
+    Anti_Tank2,
+    Tank2
+}
+
 // Game structure based on Nintendo's Advanced Wars
 export class NAW {
     public board: number[][];
@@ -21,6 +31,16 @@ export class NAW {
             [2, 2, 2, 0, 4, 2, 2, 2],
             [2, 1, 2, 2, 4, 4, 0, 2],
             [2, 1, 0, 0, 4, 4, 0, 2]
+        ];
+        this.pieces = [
+            [3, 0, 0, 0, 0, 0, 0, 6],
+            [2, 0, 0, 0, 0, 0, 0, 5],
+            [1, 0, 0, 0, 0, 0, 0, 4],
+            [1, 0, 0, 0, 0, 0, 0, 4],
+            [1, 0, 0, 0, 0, 0, 0, 4],
+            [1, 0, 0, 0, 0, 0, 0, 4],
+            [2, 0, 0, 0, 0, 0, 0, 5],
+            [3, 0, 0, 0, 0, 0, 0, 6]
         ];
     }
 
